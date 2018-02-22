985afe2190dc8fe41e11872bc89155d9b5f6def9
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-02-21 21:46:07 -0300

    Challenge 28

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -1,3 +1,5 @@
+(function() {
+  'use strict';
   /*
   No HTML:
   - Crie um formulário com um input de texto que receberá um CEP e um botão
@@ -25,3 +27,72 @@
   - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
   adicionar as informações em tela.
   */
+
+  var $form = document.querySelector('[data-js="form"]');
+  var $cepInput = document.querySelector('[data-js="cep-input"]');
+  var $alert = document.querySelector('[data-js="alert"]');
+  var $address = document.querySelector('[data-js="address"]');
+  var $logradouro = document.querySelector('[data-js="logradouro"]');
+  var $bairro = document.querySelector('[data-js="bairro"]');
+  var $cidade = document.querySelector('[data-js="cidade"]');
+  var $estado = document.querySelector('[data-js="estado"]');
+
+  $form.addEventListener('submit', onSubmit, false);
+
+  function onSubmit(event) {
+    event.preventDefault();
+
+    var cep = $cepInput.value.replace(/\D/g, '');
+
+    toggleAddress(true);
+    getCEP(cep);
+  }
+
+  function getCEP(cep) {
+    var apiUrl = 'https://viacep.com.br/ws/' + cep + '/json/';
+    var ajax = new XMLHttpRequest();
+
+    ajax.addEventListener('readystatechange', function() {
+      loading(cep);
+
+      if (this.readyState === 4 && this.status === 200) {
+         var data = JSON.parse(ajax.responseText);
+         if (data.erro) {
+          return error(cep);
+         }
+
+         return success(cep, data);
+      }
+
+      return error(cep);
+    });
+    ajax.open('GET', apiUrl, true);
+    ajax.send();
+  }
+
+  function loading(cep) {
+    $alert.innerHTML = 'Buscando informações para o CEP ' + cep + '...';
+  }
+
+  function error(cep) {
+    $alert.innerHTML = 'Não encontramos o endereço para o CEP ' + cep + '.';
+  }
+
+  function success(cep, address) {
+    $alert.innerHTML = 'Endereço referente ao CEP ' + cep + ':';
+    toggleAddress();
+    $logradouro.innerHTML = address.logradouro;
+    $bairro.innerHTML = address.bairro;
+    $cidade.innerHTML = address.localidade;
+    $estado.innerHTML = address.uf;
+  }
+
+  function toggleAddress(hidden) {
+    if (hidden) {
+      $address.classList.remove('active');
+      return;
+    }
+
+    $address.classList.toggle('active');
+  }
+})();
